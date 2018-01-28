822936cc9edc67d129ff48e7692dfd26a1e568d0
miguel-similivr <miguel@panorabbit.com>
2018-01-27 23:25:02 -0800

    Added basic user auth api

---
+++ app/Http/Controllers/Api/ApiCarsController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Car;
+use App\User;
+use Response;
+
+class ApiCarsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        if( Auth::check() ){
+            $cars = Car::where('user_id', Auth::user()->id)->get();
+            $output = [
+                'cars' => []
+            ];
+
+            foreach ($cars as $key => $car) {
+                $output['cars'][$key] =[
+                    'id' => $car->id,
+                    'make' => $car->make,
+                    'model' => $car->model,
+                    'year' => $car->year,
+                    'plate_number' => $car->plate_number
+                ];
+            }
+
+            return $output;
+            
+        }
+        
+        return response()->json([
+                'status' => 'Error'
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
